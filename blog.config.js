8c6c98e0e9df3c1289a6a0b1a890ef8409b8cb2b
LickQAQ <101987368+Lick0A0@users.noreply.github.com>
2022-03-21 08:55:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  title: '惠找资源文档中心',
+  author: '彗星',
+  email: 'LickQAQ@Gmail.com',
+  link: 'https://hzu-find.xyz',
+  description: '一项节约您大量时间的服务',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
